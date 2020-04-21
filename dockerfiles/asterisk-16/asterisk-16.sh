341dfcb66155a5481b6dd905a56623f93206140e
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-04-21 16:06:34 -0300

    MOD: start container

--- dockerfiles/asterisk-16/asterisk-16.sh
+++ dockerfiles/asterisk-16/asterisk-16.sh
@@ -1,22 +1,24 @@
 #/usr/bin/bash
 
 COMMAND=$1
+PATH=$2
 
-
-function start () {
-    docker run -d --rm --name=asterisk-16 --net=host \
-	-v ${HOME}/etc-asterisk/asterisk.conf:/etc/asterisk/asterisk.conf:rw \
-	-v ${HOME}/etc-asterisk/sip.conf:/etc/asterisk/sip.conf:rw \
-	-v ${HOME}/etc-asterisk/extensions.conf:/etc/asterisk/extensions.conf:rw \
-	-v ${HOME}/etc-asterisk/http.conf:/etc/asterisk/http.conf:rw \
-	-v ${HOME}/etc-asterisk/ari.conf:/etc/asterisk/ari.conf:rw \
+function start() {
+   /usr/bin/docker run -d --rm --name=asterisk-16-pbx --net=host \
+	-v ${PATH}/etc-asterisk/asterisk.conf:/etc/asterisk/asterisk.conf:rw \
+	-v ${PATH}/etc-asterisk/sip.conf:/etc/asterisk/sip.conf:rw \
+	-v ${PATH}/etc-asterisk/extensions.conf:/etc/asterisk/extensions.conf:rw \
+	-v ${PATH}/etc-asterisk/http.conf:/etc/asterisk/http.conf:rw \
+	-v ${PATH}/etc-asterisk/ari.conf:/etc/asterisk/ari.conf:rw \
+	-v ${PATH}/etc-asterisk/manager.conf:/etc/asterisk/manager.conf:rw \
 	-v /var/run/asterisk:/var/run/asterisk:rw \
+	-v /home/jessica/Documentos/asterisk:/home/Documentos/asterisk:rw \
 	jessicahahn/asterisk-16 asterisk -f
 
 }
 
-function stop () {
-    docker stop asterisk-16 
+function stop() {
+    /usr/bin/docker stop asterisk-16-pbx 
 }
 
 if [ $COMMAND == 'start' ];
@@ -35,5 +37,5 @@ elif [ $COMMAND == 'restart' ];
 else
     echo "Invalid Argument! not is start, stop or restart"
 fi
-
-docker exec -it asterisk-16 rasterisk
+#rasterisk
+/usr/bin/docker exec -it asterisk-16-pbx bash
