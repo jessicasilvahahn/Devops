1c0c14def1c2a6dd4ead19d2fd63fb657b65a397
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-06-20 16:52:24 -0300

    ADD: ssh file

--- dockerfiles/li-asterisk-16/li-asterisk.sh
+++ dockerfiles/li-asterisk-16/li-asterisk.sh
@@ -2,5 +2,6 @@ path=$1
 path_container=$2
 docker_image=$3
 name=$4
+path_ssh=$5
 
-docker run -it --name=$name -v $path:$path_container:rw -v /etc/localtime:/etc/localtime:ro -p 2222:2222 -p 2403:2403 -p 2402:2402 -p 8081:8081 -p 8082:8082 $docker_image bash
+docker run -it --name=$name -v $path:$path_container:rw -v /etc/localtime:/etc/localtime:ro -v $path_ssh/etc-ssh/sshd_config:/etc/ssh/sshd_config:rw -p 2222:2222 -p 2403:2403 -p 2402:2402 -p 8081:8081 -p 8082:8082 $docker_image bash
