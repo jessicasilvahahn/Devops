942121fbc307a8af0cf4a8c37b605517eaec2d2e
jessicasilvahahn <jessica.hahn.silva@gmail.com>
2020-09-25 21:16:00 -0300

    MOD: dockerfile e scripts para suporte a alteracoes realizadas na aplicacao de li

--- dockerfiles/asterisk-16/asterisk-16.sh
+++ dockerfiles/asterisk-16/asterisk-16.sh
@@ -3,23 +3,24 @@
 COMMAND=$1
 PATH=$2
 CONTAINER=$3
-MODULE=$4
+PROJECT=$4
+IMAGE=$5
 
 function start() {
-   /usr/bin/docker run -d --rm --name=asterisk-16-pbx --net=host \
+/usr/bin/docker run -d --rm --name=asterisk-16-pbx  --net=host \
 	-v ${PATH}/etc-asterisk/asterisk.conf:/etc/asterisk/asterisk.conf:rw \
 	-v ${PATH}/etc-asterisk/sip.conf:/etc/asterisk/sip.conf:rw \
 	-v ${PATH}/etc-asterisk/extensions.conf:/etc/asterisk/extensions.conf:rw \
 	-v ${PATH}/etc-asterisk/http.conf:/etc/asterisk/http.conf:rw \
 	-v ${PATH}/etc-asterisk/ari.conf:/etc/asterisk/ari.conf:rw \
 	-v ${PATH}/etc-asterisk/manager.conf:/etc/asterisk/manager.conf:rw \
-    	-v ${PATH}/etc-asterisk/cdr_manager.conf:/etc/asterisk/cdr_manager.conf:rw \
+    -v ${PATH}/etc-asterisk/cdr_manager.conf:/etc/asterisk/cdr_manager.conf:rw \
 	-v ${PATH}/etc-asterisk/cdr.conf:/etc/asterisk/cdr.conf:rw \
-	-v /var/run/asterisk:/var/run/asterisk:rw \
-	-v /home/jessica/Documentos/asterisk:/home/Documentos/asterisk:rw \
-        -v ${MODULE}:${CONTAINER}:rw \
-	-v /etc/localtime:/etc/localtime:ro \
-	jessicahahn/asterisk-16 asterisk -f
+     -v ${PATH}/etc-asterisk/keys/:/etc/asterisk/keys:rw \
+    -v /var/run/asterisk:/var/run/asterisk:rw \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v ${PROJECT}:${CONTAINER}:rw \
+    ${IMAGE} asterisk -f
 
 }
 
@@ -43,5 +44,5 @@ elif [ $COMMAND == 'restart' ];
 else
     echo "Invalid Argument! not is start, stop or restart"
 fi
-#rasterisk
+
 /usr/bin/docker exec -it asterisk-16-pbx bash
--- dockerfiles/li-asterisk-16/li-asterisk.sh
+++ dockerfiles/li-asterisk-16/li-asterisk.sh
@@ -1,7 +1,7 @@
-path=$1
-path_container=$2
-docker_image=$3
-name=$4
-path_ssh=$5
+PROJECT=$1
+CONTAINER=$2
+IMAGE=$3
+NAME=$4
+SSH=$5
 
-docker run -it --name=$name -v $path:$path_container:rw -v /etc/localtime:/etc/localtime:ro -v $path_ssh/etc-ssh/sshd_config:/etc/ssh/sshd_config:rw -p 2222:2222 -p 2403:2403 -p 2402:2402 -p 8081:8081 -p 8082:8082 $docker_image bash
+docker run -it --name=$NAME -v $PROJECT:$CONTAINER:rw -v $SSH/etc-ssh/sshd_config:/etc/ssh/sshd_config:rw --net=host $IMAGE bash
