d11bcead7191a77a5e48f421eefcdf9913f7fe79
jessicasilvahahn <jessica.hahn.silva@gmail.com>
2020-09-27 17:47:17 -0300

    FIX: tempo

--- dockerfiles/li-asterisk-16/li-asterisk.sh
+++ dockerfiles/li-asterisk-16/li-asterisk.sh
@@ -4,4 +4,4 @@ IMAGE=$3
 NAME=$4
 SSH=$5
 
-docker run -it --name=$NAME -v $PROJECT:$CONTAINER:rw -v $SSH/etc-ssh/sshd_config:/etc/ssh/sshd_config:rw --net=host $IMAGE bash
+docker run -it --name=$NAME -v $PROJECT:$CONTAINER:rw -v $SSH/etc-ssh/sshd_config:/etc/ssh/sshd_config:rw  -v /etc/localtime:/etc/localtime:ro --net=host $IMAGE bash
